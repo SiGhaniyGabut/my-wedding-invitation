8c85381947ba06ebca0c9a94dd4fe0172257531d
Abdul Hakim Ghaniy <abdulhakimghaniy@outlook.co.id>
2024-06-08 06:18:21 +0700

    Add image viewer

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import './assets/main.css'
+import 'viewerjs/dist/viewer.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Vue3Lottie from 'vue3-lottie'
+import VueViewer from 'v-viewer'
 
 import App from './App.vue'
 
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(Vue3Lottie, { name: 'LottieAnimation' })
+app.use(VueViewer)
 
 app.mount('#app')
