e80d55b740363d3ca81903538f4777a41aa69023
Abdul Hakim Ghaniy <abdulhakimghaniy@outlook.co.id>
2024-06-09 03:02:33 +0700

    Add content for "Thanks"

---
+++ src/components/contents/ContentThanks.vue
@@ -0,0 +1,30 @@
+<script setup>
+import SectionContent from '../partials/SectionContent.vue'
+import SectionContentWrapper from '../partials/SectionContentWrapper.vue'
+</script>
+
+<template>
+  <SectionContent :isCover="true" :overflowAuto="false">
+    <SectionContentWrapper>
+      <p
+        class="cormorant-garamond-bold text-4xl flex flex-col gap-2 justify-center md:flex-row md:text-5xl"
+      >
+        TERIMA KASIH
+      </p>
+      <p>
+        Kami ucapkan kepada seluruh pihak yang telah membantu dan mendukung pernikahan kami. Semoga
+        Allah SWT membalas seluruh kebaikan dengan balasan yang berlipat ganda.
+      </p>
+      <p>
+        Tak lupa, kami juga memohon doa dan restu dari handai tolan, agar pernikahan ini menjadi
+        pernikahan yang Sakinah Mawaddah Warahmah.
+      </p>
+      <p>Aamiin, Ya Rabbal 'Alamin.</p>
+      <p>
+        Hormat Kami,
+        <br />
+        <span class="text-xs">- Abdul Hakim Ghaniy &amp; Mega Apriyani Tarwiyah -</span>
+      </p>
+    </SectionContentWrapper>
+  </SectionContent>
+</template>
