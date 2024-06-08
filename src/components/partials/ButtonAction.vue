afbcb0767849bc7854fdb7ebd934a674582737eb
Abdul Hakim Ghaniy <abdulhakimghaniy@outlook.co.id>
2024-06-09 02:57:10 +0700

    Create Button Action partial

---
+++ src/components/partials/ButtonAction.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  text: String,
+  type: String
+})
+</script>
+
+<template>
+  <button
+    :type="type"
+    class="cormorant-garamond-bold text-xl outline-none border-none bg-rose-400 hover:bg-rose-200 rounded-md shadow-md transition duration-300 ease-in-out w-full p-2"
+  >
+    {{ text }}
+  </button>
+</template>
